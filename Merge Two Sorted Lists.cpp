c335cea1eaf327c8251d3f9fad6eb846bc146cb5
ChocoLa-1484 <naiyanyang@gmail.com>
2026-06-14 15:42:12 +0800

    easy, linked list

---
+++ Merge Two Sorted Lists.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* head = new ListNode(0), *dummy = head;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                dummy->next = new ListNode(list1->val);
+                list1 = list1->next;
+            } else {
+                dummy->next = new ListNode(list2->val);
+                list2 = list2->next;
+            }
+            dummy = dummy->next;
+        }
+        if (list1) {
+            dummy->next = list1;
+        } else {
+            dummy->next = list2;
+        }
+        return head->next;
+    }
+};
