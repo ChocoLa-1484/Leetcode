5b6250792d90bf325c3d60bef4d4653dcf93a44f
ChocoLa-1484 <naiyanyang@gmail.com>
2026-06-29 19:41:07 +0800

    easy, AC automaton

--- Number of Strings That Appear as Substrings in Word.cpp
+++ Number of Strings That Appear as Substrings in Word.cpp
@@ -34,3 +34,86 @@ public:
         return 0;
     }
 };
+
+class Solution {
+    static constexpr int N = 10005;
+    class ACAutomaton {
+    public: 
+        struct Node {
+            int son[26];
+            int fail;
+            vector<int> out;
+            void init() {
+                memset(son, 0, sizeof(son));
+                fail = 0;
+                out.clear();
+            }
+        } tr[N];
+        int tot;
+        void init() { 
+            tot = 0; 
+            tr[0].init();
+        }
+        void insert(const string &s, int id) {
+            int u = 0;
+            for (char ch : s) {
+                int &son = tr[u].son[ch - 'a'];
+                if (!son) {
+                    son = ++tot;
+                    tr[son].init();
+                }
+                u = son;
+            }
+            tr[u].out.push_back(id);
+        }
+        void build() {
+            queue<int> q;
+            for (int c = 0; c < 26; c++) {
+                if (tr[0].son[c]) {
+                    q.push(tr[0].son[c]);
+                }
+            }
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (int c = 0; c < 26; c++) {
+                    int v = tr[u].son[c];
+                    if (v) {
+                        tr[v].fail = tr[tr[u].fail].son[c];
+                        for (int id : tr[tr[v].fail].out)
+                            tr[v].out.push_back(id);
+                        q.push(v);
+                    } else {
+                        tr[u].son[c] = tr[tr[u].fail].son[c];
+                    }
+                }
+            }
+        }
+        int query(const string &text, int n) {
+            vector<bool> vis(n, false);
+            int ans = 0;
+            int u = 0;
+            for (char ch : text) {
+                u = tr[u].son[ch - 'a'];
+                for (int id : tr[u].out) {
+                    if (!vis[id]) {
+                        vis[id] = true;
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+    };
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        int n = patterns.size();
+        ACAutomaton AC;
+        AC.init();
+        for (int i = 0; i < n; i++) {
+            AC.insert(patterns[i], i);
+        }
+        AC.build();
+        return AC.query(word, n);
+    }
+};
