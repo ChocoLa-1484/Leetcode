046e22e6c47f2aa06177b77e6961b0151581eea1
ChocoLa-1484 <naiyanyang@gmail.com>
2026-07-12 14:59:44 +0800

    easy, dp

--- Min Cost Climbing Stairs.cpp
+++ Min Cost Climbing Stairs.cpp
@@ -3,6 +3,7 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int v1 = cost[0], v2 = cost[1];
         int n = cost.size();
+        
         for(int i = 2; i < n; i++) {
             int v = min(v1, v2) + cost[i];
             v1 = v2;
