c8ceaab894fca489867398d59e7663b06d9ab9d5
ChocoLa-1484 <naiyanyang@gmail.com>
2026-07-18 01:06:58 +0800

    easy, monotonic stack

---
+++ Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        stack<int> st;
+        int n = prices.size();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() > prices[i])
+                st.pop();
+            int x = st.empty() ? 0 : st.top();
+            st.push(prices[i]);
+            prices[i] -= x;
+        }
+        return prices;
+    }
+};
