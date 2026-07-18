640c8e6c4deeed750f67f86084a0ebbd8a7c1816
ChocoLa-1484 <naiyanyang@gmail.com>
2026-07-19 02:01:03 +0800

    easy, queue

---
+++ Implement Queue using Stacks.cpp
@@ -0,0 +1,36 @@
+class MyQueue {
+    stack <int> s1, s2;
+    int front = -1;
+public:
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if (s1.empty())
+            front = x;
+        s1.push(x);
+    }
+    
+    int pop() {
+        if (s2.empty()) {
+            while (!s1.empty()) {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int x = s2.top();
+        s2.pop();
+        return x;
+    }
+    
+    int peek() {
+        if (!s2.empty())
+            return s2.top();
+        return front;
+    }
+    
+    bool empty() {
+        return s1.empty() && s2.empty();
+    }
+};
