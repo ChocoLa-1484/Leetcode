4482c7f183103164bcfff4c22ca08a5d97c1eb90
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-06 14:40:33 +0800

    hard, backtracking+math

---
+++ Smallest Divisible Digit Product II.cpp
@@ -0,0 +1,71 @@
+class Solution {
+    int getMinDigits(array<int, 4> cnt) {
+        for (int& i : cnt)   i = max(0, i);
+        int count = cnt[0] / 3 + cnt[1] / 2 + cnt[2] + cnt[3];
+        cnt[0] %= 3;    cnt[1] %= 2;
+        if (cnt[0] == 2 && cnt[1] == 1) count += 2;
+        else if (cnt[0] >= 1 || cnt[1] >= 1)    count++;
+        return count;
+    }
+    string build(array<int, 4> cnt, int len) {
+        for (int& i : cnt)   i = max(0, i); 
+        array<int, 10> a = {};
+        a[5] = cnt[2];  a[7] = cnt[3];  a[8] = cnt[0] / 3;  a[9] = cnt[1] / 2;
+        cnt[0] %= 3;    cnt[1] %= 2;
+        if (cnt[0] >= 1 && cnt[1] >= 1) a[6]++, cnt[0]--, cnt[1]--;
+        if (cnt[0] >= 2)    a[4]++, cnt[0] -= 2;
+        a[2] += cnt[0]; a[3] += cnt[1];
+        int count = 0;
+        for (int i = 2; i < 10; i++)    count += a[i];
+        a[1] = len - count;
+        string ret = "";    ret.reserve(len);
+        for (int i = 1; i < 10; i++)
+            ret.append(a[i], i + '0');
+        return ret;
+    }
+public:
+    string smallestNumber(string num, long long t) {
+        constexpr int prime[4] = {2, 3, 5, 7};
+        auto factorize = [&](long long& t) {
+            array<int, 4> cnt = {};
+            for (int i = 0; i < 4; i++) {
+                while (t % prime[i] == 0) {
+                    cnt[i]++;
+                    t /= prime[i];
+                }
+            }
+            return cnt;
+        };
+        array<int, 4> cnt = factorize(t);
+        if (t != 1) return "-1";
+        int n = num.length();
+        vector<vector<int>> pref(4, vector<int>(n + 1));
+        int maxI = n;
+        for (int i = 0; i < n; i++) {
+            int d = num[i] - '0';
+            if (d == 0) {
+                maxI = i;break;
+            }
+            long long td = d;
+            auto fac = factorize(td);
+            for (int j = 0; j < 4; j++)
+                pref[j][i + 1] = pref[j][i] + fac[j];
+        }
+        array<int, 4> rem;
+        for (int i = 0; i < 4; i++) rem[i] = cnt[i] - pref[i][maxI];
+        if (maxI == n && getMinDigits(rem) == 0)   return num;
+        for (int i = maxI; i >= 0; i--) {
+            int st = (i < n) ? num[i] - '0' + 1 : 1;
+            for (int d = st; d <= 9; d++) {
+                long long td = d;
+                auto fac = factorize(td);
+                for (int j = 0; j < 4; j++)
+                    fac[j] = cnt[j] - pref[j][i] - fac[j];
+                int len = n - i - 1;
+                if (getMinDigits(fac) <= len)
+                    return num.substr(0, i) + to_string(d) + build(fac, len);
+            }
+        }
+        return build(cnt, max(n + 1, getMinDigits(cnt)));
+    }
+};
