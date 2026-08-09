44ba6f082646ed5177292cbc842d4221f6aba52e
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-10 01:36:10 +0800

    medium, dfs+dp

--- Stone Game II.cpp
+++ Stone Game II.cpp
@@ -1,22 +1,21 @@
 class Solution {
+    int memo[101][101];
+    int suf[101];
+    int dfs(int i, int M, const int n) {
+        if (i + 2 * M >= n) return suf[i];
+        if (memo[i][M] != -1)   return memo[i][M];
+        int MIN = INT_MAX;
+        for (int X = 1; X <= 2 * M; ++X)
+            MIN = min(MIN, dfs(i + X, max(M, X), n));
+        return memo[i][M] = suf[i] - MIN;
+    }
 public:
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        int suf = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            suf += piles[i];
-            for (int M = 1; M <= n; ++M) {
-                if (i + 2 * M >= n) {
-                    dp[i][M] = suf;
-                } else {
-                    int MIN = INT_MAX;
-                    for (int j = 1; j <= 2 * M; ++j)
-                        MIN = min(MIN, dp[i + j][max(M, j)]);
-                    dp[i][M] = suf - MIN;
-                }
-            }
-        }
-        return dp[0][1];
+        memset(memo, -1, sizeof(memo));
+        suf[n] = 0;
+        for (int i = n - 1; i >= 0; --i)
+            suf[i] = suf[i + 1] + piles[i];
+        return dfs(0, 1, n);
     }
 };
