b3c58427ef2f4e79590fa86384edfaa1bd0df90f
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-12 14:26:35 +0800

    medium, two pointers

--- 3Sum Closest.cpp
+++ 3Sum Closest.cpp
@@ -2,19 +2,34 @@ class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
-        int ans = INT_MAX;
-        for (int i = 0; i < nums.size() - 2; i++) {
-            int j = i + 1, k = nums.size() - 1;
+        int n = nums.size();
+        int ans = INT_MAX >> 1;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])    continue;
+            int s = nums[i] + nums[i + 1] + nums[i + 2];
+            if (s > target) {
+                if (s - target < abs(ans - target))
+                    ans = s;
+                break;
+            }
+            s = nums[i] + nums[n - 1] + nums[n - 2];
+            if (s < target) {
+                if (target - s < abs(ans - target))
+                    ans = s;
+                continue;
+            }
+            int j = i + 1, k = n - 1;
             while (j < k) {
-                int dif = nums[i] + nums[j] + nums[k] - target;
-                if (abs(dif) < abs(ans)) 
-                    ans = dif;
-                if (dif < 0)    j++;
-                else if (dif > 0)    k--;
-                else    return target;
-                
+                int x = nums[i] + nums[j] + nums[k];
+                if (x == target)    return target;
+                if (abs(x - target) < abs(ans - target))  
+                    ans = x;
+                if (x > target)
+                    k--;
+                else 
+                    j++;
             }
         }
-        return (ans + target);
+        return ans;
     }
 };
