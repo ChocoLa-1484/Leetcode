6d194ce2a484ea14e7b8f96a0aba5d3cd3d59224
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-12 11:46:58 +0800

    easy, hash

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map <int, int> hash;
         for (int i = 0; i < nums.size(); i++) {
-            hash[nums[i]] = i;
-        }
-        for (int i = 0; i < nums.size(); i++) {
             int t = target - nums[i];
-            if (hash.count(t) && hash[t] != i)    
+            if (hash.count(t))    
                 return {i, hash[t]};
+            hash[nums[i]] = i;
         }
         return {};
     }
