3753622e3706cd23c0ec255e1d84d5f0e38ea3f6
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-12 14:38:22 +0800

    medium, two pointers

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
+        ranges::sort(nums);
         vector<vector<int>> ans;
         for (int i = 0; i < n - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1])    continue;
+            if ((long long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if ((long long)nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
             for (int j = i + 1; j < n - 2; j++) {
                 if (j > i + 1 && nums[j] == nums[j - 1])    continue;
-                long long t = (long long)target - nums[i] - nums[j];
+                if ((long long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if ((long long)nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
                 int l = j + 1, r = n - 1;
                 while (l < r) {
-                    if (nums[l] + nums[r] < t) {
-                        l++;
-                    } else if (nums[l] + nums[r] > t) {
-                        r--;
-                    } else {
+                    long long sum = (long long)nums[i] + nums[j] + nums[l] + nums[r];
+                    if (sum < target) l++;
+                    else if (sum > target) r--;
+                    else {
                         ans.push_back({nums[i], nums[j], nums[l], nums[r]});
-                        while (l < r && nums[l] == nums[l + 1]) l++;
-                        while (l < r && nums[r] == nums[r - 1]) r--;
-                        l++, r--;
+                        for (l++; l < r && nums[l] == nums[l - 1]; l++);
+                        for (r--; r > l && nums[r] == nums[r + 1]; r--);
                     }
                 }
             }
