37eeb042377a69cae765a2841c7b2d99f3d1ad7d
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-13 09:58:02 +0800

    easy, silding window

--- Maximum Length Substring With Two Occurrences.cpp
+++ Maximum Length Substring With Two Occurrences.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int maximumLengthSubstring(string s) {
         int cnt[26] = {};
-        int l = 0;
+        int l = 0, ans = 0;
         int n = s.length();
-        int ans = 0;
         for (int r = 0; r < n; r++) {
             cnt[s[r] - 'a']++;
             while (cnt[s[r] - 'a'] > 2)
