76ad3b6e3d3fe28ecb7835fb13243fcb6241db81
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-16 12:53:04 +0800

    medium, game theory

---
+++ Stone Game IX.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {};
+        for (const int x : stones) 
+            cnt[x % 3]++;
+        if (cnt[0] % 2 == 0)
+            return cnt[1] && cnt[2] ;
+        return abs(cnt[1] - cnt[2]) > 2;
+    }
+};
