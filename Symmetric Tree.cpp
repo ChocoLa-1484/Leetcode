58a99d1c7d28f0449a26f75118b0c9bb570e82fc
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-17 14:23:51 +0800

    easy, BT symmetric

--- Symmetric Tree.cpp
+++ Symmetric Tree.cpp
@@ -3,6 +3,7 @@ public:
     bool isSymmetric(TreeNode* root) {
         return isSameTree(root->left, root->right);
     }
+    
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p || !q) return p == q;
         return p->val == q->val && isSameTree(p->left, q->right) && isSameTree(p->right, q->left);
