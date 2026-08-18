e89b233b3d8af6f3b3447cc78f4a1b5082218083
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-18 21:38:11 +0800

    medium, BT construct

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,25 +1,17 @@
 class Solution {
-private:
-    unordered_map<int, int> mp{};
-    int index = 0;
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int, int> mp;
         int n = inorder.size();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             mp[inorder[i]] = i;
-        }
-        return build(preorder, 0, n - 1);;
-    }
-private:
-    TreeNode* build(vector<int>& preorder, int l, int r) {
-        if (l > r)  return nullptr;
-        int rootVal = preorder[index++];
-        TreeNode* root = new TreeNode(rootVal);
-        int mid = mp[rootVal];
-
-        root->left = build(preorder, l, mid - 1);
-        root->right = build(preorder, mid + 1, r);
-
-        return root;
+        auto dfs = [&](this auto&& dfs, int l, int r, int id) -> TreeNode*{
+            if (l == r) return nullptr;
+            int left_size = mp[preorder[l]] - id;
+            TreeNode* left = dfs(l + 1, l + left_size + 1, id);
+            TreeNode* right = dfs(l + 1 + left_size, r, id + 1 + left_size);
+            return new TreeNode(preorder[l], left, right);
+        };
+        return dfs(0, n, 0);;
     }
 };
