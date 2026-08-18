d0a0cb84d95b56b6fbd8939ae3b10a87bd3b62bf
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-18 14:53:08 +0800

    easy, BT

---
+++ Search in a Binary Search Tree.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (!root)  return NULL;
+        if (root->val == val)   return root;
+        return val > root->val ? searchBST(root->right, val) : searchBST(root->left, val);
+    }
+};
