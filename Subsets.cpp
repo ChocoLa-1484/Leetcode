c394acc8f5b5f4ce9d56c58561b150d1ea48fcb6
ChocoLa-1484 <naiyanyang@gmail.com>
2026-08-25 14:04:38 +0800

    medium, backtracking

--- Subsets.cpp
+++ Subsets.cpp
@@ -19,3 +19,37 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> path;
+        int n = nums.size();
+        auto dfs = [&](this auto&& dfs, int i) -> void {
+            ans.emplace_back(path);
+            for (int j = i; j < n; j++) { 
+                path.push_back(nums[j]);
+                dfs(j + 1);
+                path.pop_back();
+            }
+        };
+        dfs(0);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans(1 << n);
+        for (int i = 0; i < (1 << n); i++) {
+            for (int j = 0; j < n; j++) {
+                if (i >> j & 1)
+                    ans[i].push_back(nums[j]);
+            }
+        }
+        return ans;
+    }
+};
