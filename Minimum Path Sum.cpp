434493dce9dd02f86a4e90bbc5ff45532bdccdc4
Choco_La-1484 <naiyanyang@gmail.com>
2026-09-09 18:28:55 +0800

    medium, dp

--- Minimum Path Sum.cpp
+++ Minimum Path Sum.cpp
@@ -2,14 +2,29 @@ class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        for (int j = 1; j < n; j++) 
+        for (int j = 1; j < n; j++)
             grid[0][j] += grid[0][j - 1];
         for (int i = 1; i < m; i++) {
             grid[i][0] += grid[i - 1][0];
             for (int j = 1; j < n; j++) {
-                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+                grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]); 
             }
         }
         return grid.back().back();
+    }
+};
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[1] = 0;
+        for (const auto& r : grid) {
+            for (int j = 0; j < n; j++) {
+                dp[j + 1] = r[j] + min(dp[j + 1], dp[j]);
+            }
+        }
+        return dp.back();
     }
 };
